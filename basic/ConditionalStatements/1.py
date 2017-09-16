99f31d2adcca0e3354dbc7826f15cc8d71f4d112
BinSin <binyong318@gmail.com>
2017-09-16 17:08:29 +0900

    ConditionalStatements
    if : elif : else :
    //
    if elsif else end

---
+++ basic/ConditionalStatements/1.py
@@ -0,0 +1,10 @@
+
+input = 11
+real1 = 11
+real2 = "ab"
+if real1 == input :
+	print("code1")
+elif real2 == input :
+	print("code2")
+else :
+	print("code3")
---
+++ basic/ConditionalStatements/1.rb
@@ -0,0 +1,10 @@
+input = 11
+real1 = 11
+real2 = "ab"
+if real1 == input
+	puts("code1")
+elsif real2 = input
+	puts("code2")
+else
+	puts("code3")
+end
