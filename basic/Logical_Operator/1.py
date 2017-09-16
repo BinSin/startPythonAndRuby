e96e8696ce279d22f5d3d74e7a3bd6280dbb3524
BinSin <binyong318@gmail.com>
2017-09-16 17:38:17 +0900

    Logicial Operator

---
+++ basic/Logical_Operator/1.py
@@ -0,0 +1,11 @@
+input_id = input("아이디를 입력해주세요\n")
+input_pwd = input("비밀번호를 입력해주세요\n")
+real_id = "BinSin"
+real_pwd = "1234"
+if input_id == real_id :
+	if input_pwd == real_pwd :
+		print("Hello")
+	else :
+		print("잘못된 비밀번호 입니다.")
+else :
+	print("잘못된 아이디입니다.")
