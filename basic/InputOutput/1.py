864ea19c786e0f5e5049b8dc5de63933a94e1dad
BinSin <binyong318@gmail.com>
2017-09-16 17:37:14 +0900

    Input Ouput

---
+++ basic/InputOutput/1.py
@@ -0,0 +1,9 @@
+in_str = input("입력해주세요.\n")
+real1 = "11"
+real2 = "ab"
+if in_str == real1 :
+	print("Good")
+elif in_str == real2 :
+	print("Wonderful")
+else :
+	print("Fool")
---
+++ basic/InputOutput/1.rb
@@ -0,0 +1,11 @@
+puts("입력해주세요")
+in_str = gets.chomp()
+real1 = "11"
+real2 = "ab"
+if in_str == real1
+	puts("Good")
+elsif in_str == real2
+	puts("Wonderful")
+else
+	puts("Fool")
+end
