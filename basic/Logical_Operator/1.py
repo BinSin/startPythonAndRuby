2a8457dba332df6ee0f302f8c39b52d33d1be246
BinSin <binyong318@gmail.com>
2017-09-17 18:01:04 +0900

    for Hangual

--- basic/Logical_Operator/1.py
+++ basic/Logical_Operator/1.py
@@ -1,3 +1,13 @@
+'''
+ATOM에서 한글 나오게 하기
+
+import sys
+import io
+
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
+'''
+
 input_id = input("아이디를 입력해주세요\n")
 input_pwd = input("비밀번호를 입력해주세요\n")
 real_id = "BinSin"
