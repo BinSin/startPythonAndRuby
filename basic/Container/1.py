2339ff4321655b36f57a91c3a707f4ac70328b63
BinSin <binyong318@gmail.com>
2017-09-18 09:55:58 +0900

    Container
    python은 list
    ruby는 array

---
+++ basic/Container/1.py
@@ -0,0 +1,14 @@
+#List
+name = 'BinSin'
+print(type(name))
+# type :  이 데이터의 형식을 알려준다.
+
+print(type(['BinSin', 'YongJang', 'egoing']))
+# type은 List
+names = ['BinSin', 'YongJang', 'egoing']
+print(names)
+print(names[2])
+
+BinSin = ['programmer', 'busan', 25, True]
+BinSin[1] = 'seoul'
+print(BinSin)
---
+++ basic/Container/1.rb
@@ -0,0 +1,8 @@
+#Array
+names = ['BinSin', 'YongJang', 'egoing']
+puts(names.class)
+# .class는 타입을 알려줌.
+# type은 Array
+puts(names[0])
+names[0] = 'Yong'
+print(names)
